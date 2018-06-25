b6db8c0e219452b6322327d0ffa55b6a37eae0c9
Litha <Lnkophe@gmail.com>
2018-06-25 16:44:45 +0200

    initial

---
+++ Receiver/Receiver/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Receiver
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            var facConn = new ConnectionFactory() { HostName = "localhost" };
+            using (var conn = facConn.CreateConnection())
+            {
+                using (var mod = conn.CreateModel())
+                {
+                    mod.QueueDeclare(queue: "Name", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                    var rec = new EventingBasicConsumer(mod);
+                    rec.Received += (model, ea) =>
+                      {
+                          var body = ea.Body;
+                          var msg = Encoding.UTF8.GetString(body);
+                          Console.WriteLine("Hello" + msg + ", I am your father!", msg);
+                      };
+                    mod.BasicConsume(queue: "Name", autoAck: true, consumer: rec);
+                    Console.WriteLine("Press [enter] to exit");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ Send/Send/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Send
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            var facConn = new ConnectionFactory() { HostName = "localhost" };
+            using (var conn = facConn.CreateConnection())
+            using (var mod = conn.CreateModel())
+                {
+                string n;
+                Console.WriteLine("Hello my name is,");
+                n = Console.ReadLine();
+                    mod.QueueDeclare(queue: "Name", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                    string msg = "Hello my name is, " + n;
+                    var body = Encoding.UTF8.GetBytes(msg);
+
+                    mod.BasicPublish(exchange: "", routingKey: "Name", basicProperties: null, body: body);
+                    Console.WriteLine("[x] sent {0}", msg);
+                }
+                Console.WriteLine("Press [enter] to exit");
+                Console.ReadLine();            
+        }
+    }
+}
